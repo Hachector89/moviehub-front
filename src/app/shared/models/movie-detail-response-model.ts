67d9a3c0bb2c57c2429152c2c874af501441946d
Hector <hectormoreno1989@hotmail.com>
2025-08-25 17:50:29 +0200

    movie detail model update

--- src/app/shared/models/movie-detail-response-model.ts
+++ src/app/shared/models/movie-detail-response-model.ts
@@ -26,6 +26,17 @@ export interface MovieDetail {
   video: boolean
   vote_average: number
   vote_count: number
+  credits?: Credits
+  similar?: Recommendations
+  recommendations?: Recommendations
+  reviews?: Reviews
+}
+
+export interface Collection {
+  id: number
+  name: string
+  poster_path?: string
+  backdrop_path?: string
 }
 
 export interface Genre {
@@ -51,9 +62,74 @@ export interface SpokenLanguage {
   name: string
 }
 
-export interface Collection {
-  id: number
-  name: string
-  poster_path?: string
-  backdrop_path?: string
+export interface Credits {
+  cast: Cast[];
+  crew: Cast[];
 }
+
+export interface Cast {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: null | string;
+  cast_id?: number;
+  character?: string;
+  credit_id: string;
+  order?: number;
+  department?: string;
+  job?: string;
+}
+
+export interface Recommendations {
+  page: number;
+  results: RecommendationsResult[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface RecommendationsResult {
+  adult: boolean;
+  backdrop_path?: string;
+  id: number;
+  title: string;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  media_type?: string;
+  original_language: string;
+  genre_ids: number[];
+  popularity: number;
+  release_date: Date;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface Reviews {
+  page: number;
+  results: ReviewsResult[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface ReviewsResult {
+  author: string;
+  author_details: AuthorDetails;
+  content: string;
+  created_at: Date;
+  id: string;
+  updated_at: Date;
+  url: string;
+}
+
+export interface AuthorDetails {
+  name: string;
+  username: string;
+  avatar_path: null | string;
+  rating: number;
+}
+
